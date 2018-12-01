6a5654510f475b42992cf9d3f5261d9b4217b095
Gr2_srk <gvjohan99@gmail.com>
2018-12-01 00:08:55 -0600

    fix delay

--- BOMBA_GR2.ino
+++ BOMBA_GR2.ino
@@ -6,7 +6,6 @@
 
 const int led1=2; //LEDS de control
 const int led2=3;
-const int led3=4;
 const int rele=5;// Relevador
 int Nivel;
 
@@ -14,27 +13,26 @@ void setup() {
   Serial.begin(9600); //Se inicializa la comunicación
   pinMode(led1,OUTPUT); //pin como salida
   pinMode(led2,OUTPUT); //pin como salida
-  pinMode(led3,OUTPUT); //pin como salida
   pinMode(rele,OUTPUT); //pin como salida
   digitalWrite(led1, HIGH); // Indicador de encendido
-  digitalWrite(led3,HIGH); // Indicador modo espera
 }
 
 void loop() {
   
   Serial.println(analogRead(A5)); //Depuración
   Nivel=analogRead(A5);
-  
+  int i=0;
   if(Nivel>620){ //Tanque lleno 
-    digitalWrite(led3,LOW); //Apagar modo espera
-    digitalWrite(led2,HIGH); //Indicador de bomba encendida
+    relevador=millis();
     digitalWrite(rele,HIGH);//Encender bomba
-    delay(240000000); //4 minutos de bombeo
+    for(i=1;i<=240;i++){
+      Serial.print(i);
+      Serial.print(" | \n");
+      delay(1000);
+      }
     digitalWrite(rele,LOW);//Apagar bomba
     digitalWrite(led2,LOW); //Bomba apagada
-    digitalWrite(led3,HIGH); // Indicador modo espera
+    Nivel=0;
     }
-    
-  delay(100); //100 micorsegundos entre ciclo de verificación
 
 }
