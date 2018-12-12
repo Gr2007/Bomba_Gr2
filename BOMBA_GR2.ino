b922a275ac84370a5a1ace448b6e6274d60246d1
Gr2_srk <gvjohan99@gmail.com>
2018-12-11 20:48:46 -0600

    Mayor update
    cambio del ciclo for de delays al uso de la función millis, mi anterior código estaba culero.

--- BOMBA_GR2.ino
+++ BOMBA_GR2.ino
@@ -1,38 +1,60 @@
 /*
  * Programa para control de bomba
  * Granados Villeda Johan Raciel
- * Alpha 5
+ * Versión 1.5
 */
 
-const int led1=2; //LEDS de control
-const int led2=3;
-const int rele=5;// Relevador
-int Nivel;
+
+//CONSTANTES
+const long intervalo = 3000;  //Intervalo de tiempo de la bomba
+const int led1 = 2; //LED de control
+const int rele = 5;// Relevador
+
+//VARIABLES GLOBALES
+unsigned long tiempo1 = 0; //Para millis, sustitucion de ciclo for con delays      
+bool estado = true; //para definir estado de la bomba. TRUE = apagado, FALSE = encendido. 
+int Nivel = 0; //Para lectura Analogica
 
 void setup() {
   Serial.begin(9600); //Se inicializa la comunicación
   pinMode(led1,OUTPUT); //pin como salida
-  pinMode(led2,OUTPUT); //pin como salida
   pinMode(rele,OUTPUT); //pin como salida
-  digitalWrite(led1, HIGH); // Indicador de encendido
 }
 
 void loop() {
-  
-  Serial.println(analogRead(A5)); //Depuración
-  Nivel=analogRead(A5);
-  int i=0;
-  if(Nivel>620){ //Tanque lleno 
-    digitalWrite(rele,HIGH);//Encender bomba
-    digitalWrite(led2,HIGH); //Bomba encendida
-    for(i=1;i<=240;i++){
-      Serial.print(i);
-      Serial.print(" | \n");
-      delay(1000);
-      }
-    digitalWrite(rele,LOW);//Apagar bomba
-    digitalWrite(led2,LOW); //Bomba apagada
-    Nivel=0;
-    }
 
+  if (estado) //BOMBA APAGADA
+  {
+    Serial.println(analogRead(A2)); // Depuración, revisión de estado del nivel en Serial
+    Nivel = analogRead(A2); // Asignación del nivel
+   
+    if(Nivel > 500) //Control de nivel
+      encender(); // FUNCION DE ENCENDIDO DE BOMBA
+  }
+
+  if(!estado)//BOMBA ENCENDIDA
+  {
+    if(millis() > (tiempo1 + intervalo)) // Para el tiempo 
+      apagar(); // FUNCION DE APAGADO DE BOMBA
+
+    if(tiempo1 !=0) // DEPURACION DEL TIEMPO 
+      Serial.println(millis() - tiempo1); // tiempo entre encendio y apagado
+    
+  }
+}
+
+void encender() 
+{
+  tiempo1 = millis(); // INICIO DEL CONTEO 
+  digitalWrite(rele,HIGH);// Encender bomba
+  digitalWrite(led1,HIGH); // Bomba encendida
+  estado = false; // Para control de la bomba, revisar constantes
+}
+
+void apagar()
+{
+  tiempo1 = 0; // reiniciar contador
+  digitalWrite(rele,LOW);// Apagar bomba
+  digitalWrite(led1,LOW); // Bomba apagada
+  estado = true; // para control de la bomba, revisar constantes
 }
